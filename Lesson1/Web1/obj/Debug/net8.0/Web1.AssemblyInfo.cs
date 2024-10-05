819ab4cafedf076c4154149665c65666bbda2878
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-05 12:11:41 +0700

    lesson1

--- Lesson1/Web1/obj/Debug/net8.0/Web1.AssemblyInfo.cs
+++ Lesson1/Web1/obj/Debug/net8.0/Web1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40e0a3307806664dbc8dd321316251c75dbf09f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ Lesson1/Web2/Controllers/GreenwichController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Web2.Controllers
+{
+    public class GreenwichController : Controller
+    {
+        //default URL
+        //localhost:PORT/Greenwich/Index
+        //localhost:PORT/Greenwich
+
+        //custom URL
+        //localhost:PORT/GreenwichVietnam
+
+        [Route("/GreenwichVietnam")]
+        public IActionResult Index()
+        {
+            //Views/Greenwich/Index.cshtml
+            return View();
+        }
+
+        //localhost:PORT/Greenwich/Hanoi
+        public IActionResult HaNoi()
+        {
+            //Views/Greenwich/HaNoi.cshtml
+            return View();
+        }
+
+        public IActionResult HCMCity()
+        {
+            return View();
+        }
+
+        public IActionResult DaNang()
+        {
+            return View();
+        }
+
+        public IActionResult CanTho()
+        {
+            return View();
+        }
+    }
+}
