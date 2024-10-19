a17a40cbfaaba86725ad5e0c4aff88511386f326
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-19 12:16:40 +0700

    tut1

---
+++ Tutorial1/Data/Migrations/20241019032445_create_table.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Tutorial1.Data.Migrations
+{
+    public partial class create_table : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ Tutorial1/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tutorial1.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required (ErrorMessage = "Tiêu đề sách không được để trống")]
+        public string Title { get; set; }
+
+        [MinLength(3, ErrorMessage = "Tên tác giả tối thiểu 3 ký tự")]
+        [MaxLength(15, ErrorMessage = "Tên tác giả tối đa 15 ký tự")]
+        public string Author { get; set; }
+    }
+}
