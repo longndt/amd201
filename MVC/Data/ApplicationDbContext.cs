f102c1af9f7b13e0c3ad6695d992464be6512b3c
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-10-26 09:42:45 +0700

    mvc

---
+++ MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using MVC.Models;
+
+namespace MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<MVC.Models.Genre> Genre { get; set; }
+        public DbSet<MVC.Models.Book> Book { get; set; }
+    }
+}
---
+++ MVC/Models/Book.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC.Models
+{
+    public class Book
+    {
+        public int BookId { get; set; }
+        public string BookTitle { get; set; }
+        public double BookPrice { get; set; }
+        public string BookImage { get; set; }
+
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
