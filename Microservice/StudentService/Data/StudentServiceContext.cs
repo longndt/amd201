2e99bd538ae33beea0601cc92c6f32ff207bbbea
Nguyen Dinh Tran Long <longndt226@gmail.com>
2024-11-20 16:39:51 +0700

    microservice

---
+++ Microservice/StudentService/Data/StudentServiceContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using StudentService.Models;
+
+namespace StudentService.Data
+{
+    public class StudentServiceContext : DbContext
+    {
+        public StudentServiceContext (DbContextOptions<StudentServiceContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<StudentService.Models.Student> Student { get; set; } = default!;
+    }
+}
